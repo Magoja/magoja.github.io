4d1a939ed7eef8ac912c04d3db62b97ac1f25047
Magoja <magoja@gmail.com>
2025-09-01 16:24:41 +0000

    Deploying to gh-pages from @ Magoja/magoja.github.io@8dcf8ae90a91d722c08072d2f786bd6087c0b5fe 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,8 +16,8 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/blog/";
           },
-        },{id: "nav-jakeutil",
-          title: "Jakeutil",
+        },{id: "nav-back-to-jakeutil",
+          title: "Back to Jakeutil",
           description: "",
           section: "Navigation",
           handler: () => {
